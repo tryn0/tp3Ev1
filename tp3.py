a7f0053d1c09b8d26b25a2f931114b14d6d303cd
tryn0 <vkae@live.com>
2018-11-13 21:18:49 +0100

    tp3 from sys import argv y funciones fixed

--- tp3.py
+++ tp3.py
@@ -1,9 +1,9 @@
-import sys
+from sys import argv
 
-fh = open(sys.argv[1],"r")
-line = fh.readline()
-while line:
-  print(line,end="")
-  line = fh.readline()
-fh.close()
+leer = open(argv[1],"r")
+linea = leer.readline()
+while linea:
+  print(linea,end="")
+  linea = leer.readline()
+leer.close()
 
